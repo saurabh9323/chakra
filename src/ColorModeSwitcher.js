6ff62c118af5b816d25ba5994bc5be5b9c7023b9
saurabh592 <shubham.pathak9323@gmail.com>
2023-04-04 11:04:28 +0530

    adding some files

--- src/ColorModeSwitcher.js
+++ src/ColorModeSwitcher.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
 import { FaMoon, FaSun } from 'react-icons/fa';
 
-export const ColorModeSwitcher = props => {
+ const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
   const SwitchIcon = useColorModeValue(FaMoon, FaSun);
@@ -10,14 +10,18 @@ export const ColorModeSwitcher = props => {
   return (
     <IconButton
       size="md"
-      fontSize="lg"
       aria-label={`Switch to ${text} mode`}
       variant="ghost"
       color="current"
       marginLeft="2"
+      pos={'fixed'}
+      top={"4"}
+      right={'4'}
       onClick={toggleColorMode}
       icon={<SwitchIcon />}
       {...props}
     />
   );
 };
+
+export default ColorModeSwitcher;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { ColorModeScript, ChakraProvider} from '@chakra-ui/react';
+import { ColorModeScript, ChakraProvider, theme} from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
+import ColorModeSwitcher from './ColorModeSwitcher';
 
 
 const container = document.getElementById('root');
@@ -10,7 +11,8 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ColorModeScript />
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+    <ColorModeSwitcher />
     <App />
     </ChakraProvider>
   </StrictMode>
